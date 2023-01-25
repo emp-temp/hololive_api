1885b8fce97af276c9557dfbbcc08675e4a648a7
emp-temp <emptemp0000@gmail.com>
2023-01-26 01:18:39 +0900

    feat: add handler function(get_student, get_term_students)

--- crates/server/src/handler.rs
+++ crates/server/src/handler.rs
@@ -1,6 +1,22 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse, Responder};
 
 #[get("/health")]
 pub async fn health() -> impl Responder {
     HttpResponse::Ok().body("PONG")
+}
+
+// TODO: #[get("/api/{term: int}")]の中身を実装
+#[get("/api/{term}")]
+pub async fn get_term_students(path: web::Path<(u64,)>) -> impl Responder {
+    let term = path.into_inner().0;
+    HttpResponse::Ok().body(format!("term is {}", term))
+}
+
+// TODO: #[get("/api/{term: int}/{name: String}")]の中身を実装
+#[get("/api/{term}/{name}")]
+pub async fn get_student(path: web::Path<(u64, String)>) -> impl Responder {
+    let path = path.clone();
+    let term = path.0;
+    let name = path.1;
+    HttpResponse::Ok().body(format!("term is {}, name is {}", term, name))
 }
--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -10,8 +10,13 @@ async fn main() -> std::io::Result<()> {
 
     let addr = env::var("ADDR").expect("ADDR is must be set");
 
-    HttpServer::new(|| App::new().service(handler::health))
-        .bind(addr)?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(handler::health)
+            .service(handler::get_student)
+            .service(handler::get_term_students)
+    })
+    .bind(addr)?
+    .run()
+    .await
 }
